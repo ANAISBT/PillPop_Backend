3c35953d59552ac99018b93dc7ecae3293c19589
Anais Milagros <anais.bustamante@paku.pe>
2024-10-08 12:58:44 -0500

    connect

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import express from 'express'; 
+import pg from 'pg';
+import { connectionString } from 'pg/lib/defaults';
+import { config } from 'dotenv';
+
+config()
 
 const app = express()
+
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl:true
+})
 
 app.get('/',(req,res)=> {
     res.send('Hello Word')
 })
 
-app.listen(3000)
-console.log('Server on port',3000)
+app.get('/ping', async (req, res) => {
+    const result = await pool.query('SELECT NOW()')
+    return res.json(result.rows[0])
+  });
 
+// Iniciar el servidor
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+  });
